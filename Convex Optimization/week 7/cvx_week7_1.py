8c218a24017122e7748514574c9c1b2b377bb204
Matin-Noohnezhad <matinnouhnejad@gmail.com>
2021-02-02 13:58:35 +0330

    update all

--- Convex Optimization/week 7/cvx_week7_1.py
+++ Convex Optimization/week 7/cvx_week7_1.py
@@ -1,4 +1,3 @@
-# clear all; close all;
 import numpy as np
 import matplotlib.pyplot as plt
 import cvxpy as cp
@@ -17,10 +16,7 @@ ytrue = np.cumsum(ytrue)
 # and add Gaussian noise
 h = np.array([1, -0.85, 0.7, -0.3])
 k = len(h)
-# print(h.shape)
-# print(xtrue.shape)
 yhat = np.convolve(h, ytrue)
-print(yhat.shape)
 y = yhat[:-3] + np.array(
     [-0.43, -1.7, 0.13, 0.29, -1.1, 1.2, 1.2, -0.038, 0.33, 0.17, -0.19, 0.73, -0.59, 2.2, -0.14, 0.11, 1.1, 0.059,
      -0.096, -0.83, 0.29, -1.3, 0.71, 1.6, -0.69, 0.86, 1.3, -1.6, -1.4, 0.57, -0.4, 0.69, 0.82, 0.71, 1.3, 0.67, 1.2,
@@ -28,22 +24,29 @@ y = yhat[:-3] + np.array(
      -0.64, 0.38, -1, -0.02, -0.048, 4.3e-05, -0.32, 1.1, -1.9, 0.43, 0.9, 0.73, 0.58, 0.04, 0.68, 0.57, -0.26, -0.38,
      -0.3, -1.5, -0.23, 0.12, 0.31, 1.4, -0.35, 0.62, 0.8, 0.94, -0.99, 0.21, 0.24, -1, -0.74, 1.1, -0.13, 0.39, 0.088,
      -0.64, -0.56, 0.44, -0.95, 0.78, 0.57, -0.82, -0.27])
-print(y.shape)
 
-w = cp.Variable(2)
-t = np.arange(N)
-t = np.vstack((np.ones(N), t))
-t = t.reshape(2, 100)
-objective = cp.Minimize(cp.sum_squares(w @ t - y))
-constraints = [w >= 0, w @ t >= 0]
+#maximum likelihood estimation with no monotonicity taken in to account. can be solved analytically
+y_ls = cp.Variable(N)
+yhat = cp.conv(h, y_ls)[:-3]
+y = y.reshape(-1,1)
+objective = cp.Minimize(cp.sum_squares(yhat - y))
+prob = cp.Problem(objective)
+_ = prob.solve()
+error = np.sum((yhat.value - ytrue)**2)
+print('error for y_ml,free is ', error)
+
+#monotonic and non-negative signal estimation
+y_mono = cp.Variable(N)
+yhat = cp.conv(h, y_mono)[:-3]
+objective = cp.Minimize(cp.sum_squares(yhat - y))
+constraints = [y_mono[0] >= 0, y_mono[:-1] <=y_mono[1:]]
 prob = cp.Problem(objective, constraints)
-# prob = cp.Problem(objective)
-error = prob.solve()
-print(w.value)
-print('error is ', error)
+_ = prob.solve()
+error = np.sum((yhat.value - ytrue)**2)
+print('error for y_ml is ', error)
 
-y_approx = w.value @ t
-plt.plot(y_approx)
-plt.plot(ytrue)
-plt.plot(y)
+plt.plot(ytrue, label='ytrue')
+plt.plot(y_ls.value, label='y_ml,free')
+plt.plot(y_mono.value, label='y_ml')
+plt.legend()
 plt.show()
--- Convex Optimization/week 7/cvx_week7_2.py
+++ Convex Optimization/week 7/cvx_week7_2.py
@@ -1,9 +1,45 @@
 import numpy as np
+from scipy.stats import norm
 import cvxpy as cp
+import matplotlib.pyplot as plt
+# import plotly.graph_objects as go
 
+mu1 = 8
+mu2 = 20
+sigma1 = 6
+sigma2 = 17.5
+rho = -0.25
 
-mu = cp.Variable(2)
-sigma = cp.Variable(2) #standard deviation
-ro = cp.Variable(1) #correlation
+# assuming jointly gaussian distribution
+mu = mu1 + mu2
+sigma = np.sqrt(sigma1 ** 2 + sigma2 ** 2 + 2 * rho * sigma1 * sigma2)
+p_loss = norm.cdf(0, mu, sigma)
 
-r = np.arange(-30, 71)
+n = 100
+r_min = -30
+r_max = 70
+
+# discretize outcomes of R1 and R2
+r = np.linspace(r_min, r_max, n)
+
+# marginal distributions
+p1 = norm(mu1, sigma1).pdf(r)
+p1 = p1/np.sum(p1)
+p2 = norm(mu2, sigma2).pdf(r)
+p2 = p2/np.sum(p2)
+
+# form mask of region where R1 + R2 <= 0
+r1p = r * np.ones((1, n))
+r2p = r1p.T
+loss_mask = (r1p + r2p <= 0).T
+
+P = cp.Variable((n,n))
+objective = cp.Maximize(cp.sum(cp.sum(P[loss_mask])))
+constraints = [P >= 0, cp.sum(P,1)==p1, cp.sum(P,0) == p2, (r-mu1).T @ P @ (r-mu2) == rho*sigma1*sigma2]
+prob = cp.Problem(objective, constraints)
+
+result = prob.solve()
+print(result)
+
+P = P.value
+x = np.meshgrid(r1p, r2p, P.T)
